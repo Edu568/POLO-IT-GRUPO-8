90736669d6e1d20f61943e9fb294a2819f138c6c
javiRodrigue <agudtinjava@gmail.com>
2025-09-24 15:21:45 -0300

    consulta SQL para listar objetos

---
+++ Backend/src/models/models.publicaciones.js
@@ -0,0 +1,21 @@
+const db = require('../config/config.conexion');
+
+function getAllPublicaciones(callback) {
+  const sql = `
+    SELECT 
+      p.titulo,
+      p.descripcion,
+      p.estado,
+      p.disponible,
+      p.fecha_publicacion,
+      u.nombre AS usuario,
+      c.nombre AS categoria,
+      f.url AS foto
+    FROM Publicacion p
+    JOIN Usuario u ON p.id_dueno = u.id
+    JOIN Categoria c ON p.id_categoria = c.id
+    LEFT JOIN Foto f ON f.id_publicacion = p.id
+    ORDER BY p.fecha_publicacion DESC
+  `;
+  db.all(sql, [], callback);
+}
