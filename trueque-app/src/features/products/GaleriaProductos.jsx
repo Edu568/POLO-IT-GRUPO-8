df6d5673bb887293f3301b39143cbc565b8ffc9b
Rodrigo Garayalde Borges <rodrii.gborges@gmail.com>
2025-10-05 18:28:20 -0300

    se corrige estilos

--- trueque-app/src/features/products/GaleriaProductos.jsx
+++ trueque-app/src/features/products/GaleriaProductos.jsx
@@ -2,22 +2,25 @@ import { ProductoCard } from "./ProductoCard";
 import { Container, Row, Col } from "react-bootstrap";
 
 export const GaleriaProductos = () => {
+
+  /* lista de productos */
+
   return (
-    <Container fluid className="justify-content-center">
-      <Row className="justify-content-center">
-        <Col xs={12} md={4}>
+    <Container>
+      <Row>
+        <Col className="d-flex justify-content-center">
           <ProductoCard
             nombre="Producto 1"
             descripcion="Descripción del producto 1"
           />
         </Col>
-        <Col xs={12} md={4}>
+        <Col className="d-flex justify-content-center">
           <ProductoCard
             nombre="Producto 2"
             descripcion="Descripción del producto 2"
           />
         </Col>
-        <Col xs={12} md={4}>
+        <Col className="d-flex justify-content-center">
           <ProductoCard
             nombre="Producto 3"
             descripcion="Descripción del producto 3"
