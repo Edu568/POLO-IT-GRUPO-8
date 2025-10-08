deaa01314ca0112661254931ddf5ec4bf744e1eb
javiRodrigue <agudtinjava@gmail.com>
2025-10-08 12:46:08 -0300

    correciones

--- Backend/src/Server.js
+++ Backend/src/Server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('Cors')
+const cors = require('cors')
 const app = express();
 const PORT = 3000;
 const publicacionesRouter = require('./routes/publicacionesRouter');
@@ -11,17 +11,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/publicaciones", publicacionesRouter);
 app.use('/api/transacciones', transaccionRouter);
-
-// // Middleware 404 (después de routes)
-// app.use('/*', (req, res) => {
-//   res.status(404).json({ error: 'Endpoint no encontrado' });
-// });
-
-// // Middleware errores (último)
-// app.use((err, req, res, next) => {
-//   console.error(err.stack);
-//   res.status(err.status || 500).json({ error: { message: err.message } });
-// });
 
 app.listen(PORT, () => {
   console.log('Corriendo en http://localhost:' + PORT);
