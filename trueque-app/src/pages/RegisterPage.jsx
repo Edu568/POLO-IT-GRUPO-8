c25eb0ff00369cf936ddaa91b503cac7e9162e44
Edu568 <eduardo.arevalo072@gmail.com>
2025-10-22 21:56:54 -0300

    se modifica register page llamado a api

--- trueque-app/src/pages/RegisterPage.jsx
+++ trueque-app/src/pages/RegisterPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
-import { registerUser } from "../features/users/UserProfile";
+import { registerUser } from "../API/userApi";
 
 
 
